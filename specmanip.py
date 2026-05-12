6db8db8f07103cd382beb9f667fadba3b5fd55c8
Eddy Meals <edimea211@gmail.com>
2026-05-12 16:15:41 -0700

    Use correct file for orig_sum and orig_avg, clean up CLI with one (1) subcommand

--- specmanip.py
+++ specmanip.py
@@ -2,11 +2,24 @@ import sys
 import argparse
 import numpy as np
 
-def get_col(csv_path, col=4, skip=5):
+def get_col(csv_path, col, skip):
   with open(csv_path, 'r') as f:
     for _ in range(skip):
       next(f)
-    return np.array([float(line.split(',')[col]) for line in f])
+    try:
+      return np.array([float(line.split(',')[col]) for line in f])
+    except IndexError:
+      return None
+
+def read_info(csv_path):
+  with open(csv_path, 'r') as f:
+    next(f)
+    try:
+      intime = int(next(f).split(',')[1])
+      avgs = int(next(f).split(',')[1])
+    except IndexError as e:
+      raise RuntimeError(f'"{csv_path}" does not appear to be a Spectrum Studio spectrum CSV.')
+  return (intime, avgs)
 
 def replace_col(orig_path, out_path, replace_cols, skip=5):
   cols = [rc[0] for rc in replace_cols]
@@ -25,29 +38,44 @@ def replace_col(orig_path, out_path, replace_cols, skip=5):
         fields[col] = str(float(new_val))
       out.write(','.join(fields).strip() + '\n')
 
+def replace_background_subcmd(args):
+  background = get_col(args.background, 4, 5)
+  if background is None:
+    raise RuntimeError(f'"{args.background}" does not contain a background column.')
+  orig_sum = get_col(args.orig, 2, 5)
+  orig_avg = get_col(args.orig, 3, 5)
+  orig_bg = get_col(args.orig, 4, 5)
+  if orig_avg is None:
+    raise RuntimeError(f'"{orig_avg}" does not appear to be a Spectrum Studio spectrum CSV.')
+
+  _, orig_num_avgs = read_info(args.orig)
+  if orig_bg is not None:
+    orig_avg += orig_bg
+    orig_sum += orig_num_avgs * orig_bg
+
+  replace_col(
+    args.orig,
+    args.output,
+    (
+      (2, orig_sum - orig_num_avgs * background),
+      (3, orig_avg - background),
+      (4, background),
+    ),
+  )
+
 def main(arg_list):
   parser = argparse.ArgumentParser(
     prog='specmanip.py',
     description='Manipulates spectrum CSVs produced by Spectrum Studio',
   )
-  parser.add_argument('orig')
-  parser.add_argument('background')
-  parser.add_argument('output')
+  subparsers = parser.add_subparsers(required=True)
+  replace_background_parser = subparsers.add_parser('replace-background')
+  replace_background_parser.set_defaults(subcmd_func=replace_background_subcmd)
+  replace_background_parser.add_argument('orig')
+  replace_background_parser.add_argument('background')
+  replace_background_parser.add_argument('output')
   args = parser.parse_args(arg_list)
-  background = get_col(args.background)
-  orig_sum = get_col(args.background, 2)
-  new_sum = orig_sum - 3 * background
-  orig_avg = get_col(args.background, 3)
-  new_avg = orig_avg - background
-  replace_col(
-    args.orig,
-    args.output,
-    (
-      (2, new_sum),
-      (3, new_avg),
-      (4, background),
-    ),
-  )
+  args.subcmd_func(args)
 
 if __name__ == '__main__':
   main(sys.argv[1:])
